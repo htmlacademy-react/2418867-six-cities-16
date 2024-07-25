e1fd0e2afff8ecf6c67cd2f09bbea09d3f5e4d18
Tiizara <135403209+Tiizara@users.noreply.github.com>
2024-07-25 17:07:46 +0500

    Module 6.1

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { SetStateAction, useEffect, useState } from 'react';
 import { Logo } from '../../components/logo';
 import { Map } from '../../components/map';
 import { Point } from '../../types/point';
@@ -7,6 +7,7 @@ import { useAppDispatch, useAppSelector } from '../../store/hook';
 import { fetchUsers } from '../../store/action-creator';
 import OffersContainer from '../../components/offers/offers-container';
 import { ListOfCites } from '../../components/list-of-cities';
+import { Popular } from '../../components/popular';
 
 function Main(): JSX.Element {
   const dispatch = useAppDispatch();
@@ -42,6 +43,8 @@ function Main(): JSX.Element {
 
     setSelectedPoint(currentPoint);
   };
+
+  const [tab, setPlace] = useState(-1);
 
   return (
     <div className="page page--gray page--main">
@@ -79,33 +82,10 @@ function Main(): JSX.Element {
           <div className="cities__places-container container">
             <section className="cities__places places">
               <h2 className="visually-hidden">Places</h2>
-              <b className="places__found">{cityValue.length} places to stay in Amsterdam</b>
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex={0}>
-                  Popular
-                  <svg className="places__sorting-arrow" width={7} height={4}>
-                    <use xlinkHref="#icon-arrow-select" />
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom places__options--opened">
-                  <li
-                    className="places__option places__option--active"
-                    tabIndex={0}
-                  >
-                    Popular
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Price: low to high
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Price: high to low
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Top rated first
-                  </li>
-                </ul>
-              </form>
+              <b className="places__found">
+                {cityValue.length} places to stay in Amsterdam
+              </b>
+              <Popular tab={tab} onClickTab={(id: SetStateAction<number>) => setPlace(id)} />
               <OffersContainer
                 onListItemHover={handleListItemHover}
                 className={'cities'}
